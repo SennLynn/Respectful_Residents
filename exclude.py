7ab5871d87301b9d533ccb07ccfbd795a824ab95
Ethan Zhou <131318416+SennLynn@users.noreply.github.com>
2024-10-17 10:28:05 -0700

    Update exclude.py

--- exclude.py
+++ exclude.py
@@ -20,6 +20,24 @@ while dog:
   else:
       print("Please enter a valid response (y/n)")  # Prompt again if the input is invalid
 
+print("number guesser!")
+import random
+number = random.randint(1, 100)
+print("what do you think the number is(1-100), you get 5 tries")
+x = 0
+while (x <= 5 or guess == number):
+  guess = int(input("Guess number " + x + " :"))
+  if (guess < number):
+    print("too small")
+  elif (guess > number):
+    print("too big")
+  elif (guess == number):
+    print("yay you got it!")
+  else:
+    print("please insert an actual number!")
+
+
+
 
 print("heads or tails")
 new_number = input("Do you think it is heads or tails, (1 is tails, 2 is heads)")
